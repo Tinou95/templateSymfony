d8b5860303c2893145da0bcda7cc2be55d93f9df
Tinou95 <quentin95170@hotmail.fr>
2024-12-16 15:09:47 +0100

    feat: controllerAdminSubscirte

--- src/Controller/Other/SubscribeController.php
+++ src/Controller/Other/SubscribeController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller\Other;
 
+use App\Repository\SubscriptionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,8 +12,13 @@ use Symfony\Component\Routing\Attribute\Route;
 class SubscribeController extends AbstractController
 {
     #[Route('/subscribe', name: 'page_subscribe')]
-    public function index(): Response
-    {
-        return $this->render('other/abonnements.html.twig');
+    public function index(
+        SubscriptionRepository $subscriptionRepository,
+    ): Response {
+        $subscriptions = $subscriptionRepository->findAll();
+
+        return $this->render('other/abonnements.html.twig', [
+            'subscriptions' => $subscriptions,
+        ]);
     }
 }
